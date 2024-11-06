8a9e70cddc548b1d61f369ecc5d766f00ff3dd83
Amoyzzz <dharkarajveer@gmail.com>
2024-11-06 17:54:16 -0500

    im proud of how well i did this
    im proud of how well i did this

---
+++ Week 3/11-6-24/KokoEatingBananas.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def minEatingSpeed(self, piles, h):
+        def check(val):
+            hours_taken = 0
+            for pile in piles:
+                hours_taken += (pile + val - 1)//val
+            return hours_taken <= h
+        def binarysearch(lo, hi):
+            hi+=1
+            while lo < hi:
+                mid = (lo + hi) // 2
+                if check(mid):
+                    hi = mid
+                else:
+                    lo = mid + 1
+            return lo
+        return binarysearch(0, sum(piles))
+
+            
---
+++ Week 3/11-6-24/notes.md
@@ -0,0 +1,7 @@
+## Notes
+
+this like angry cows, or like any bin search silver problem. honestly pretty cool
+
+im suprised with myself getting this one so fast, like I actually wrote the binary search(except I did lo-=1 instead of h+=1 but its fine)
+
+anyway simply we binary search for the minimum k that can pass check(aka work).
